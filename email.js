3cb093eec362aed1f0b7045e670af00e0eb31b41
Michael Spagnolo <mspagnolo27@gmail.com>
2022-06-10 13:04:20 -0400

    if for email task

--- email.js
+++ email.js
@@ -29,31 +29,34 @@ let convertDate = (yyyyMMdd) => {
 
 const currentMonth = new Date().getMonth()
 const currentDay = new Date().getDate()
-
-
+ if (currentDay===11){
   User.find().lean().exec({}, function (err, results) {
-      if (err) {
-          console.log(err)
-      } else {
-          const todaysBirthdays = results.filter((a) => (convertDate(a.birthday).getMonth() == currentMonth &&
-               convertDate(a.birthday).getDate() == currentDay))
+    if (err) {
+        console.log(err)
+    } else {
+        const todaysBirthdays = results.filter((a) => (convertDate(a.birthday).getMonth() == currentMonth &&
+             convertDate(a.birthday).getDate() == currentDay))
 
-          const monthBirthdays = results.filter((a) => (new Date(a.birthday).getMonth() == currentMonth))
-          const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
-          const currentMonthName = months[currentMonth]
+        const monthBirthdays = results.filter((a) => (new Date(a.birthday).getMonth() == currentMonth))
+        const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+        const currentMonthName = months[currentMonth]
 
-          const header = `<h2> ${currentMonthName} Birthdays </h2>`   
-          const lines = monthBirthdays.map((a)=> `<div> ${a.firstname+' '+a.lastname} on ${a.birthday} </div>`).join('');
-          const html =  header + lines
+        const header = `<h2> ${currentMonthName} Birthdays </h2>`   
+        const lines = monthBirthdays.map((a)=> `<div> ${a.firstname+' '+a.lastname} on ${a.birthday} </div>`).join('');
+        const html =  header + lines
 
-          const title = `Birthday Reminder ${currentMonthName}`
-          request.post(process.env.TRUSTIFI_URL + '/api/i/v1/email', {
-          headers: {
-          'x-trustifi-key': process.env.TRUSTIFI_KEY,
-          'x-trustifi-secret': process.env.TRUSTIFI_SECRET
-        },
-        json:{"recipients":[{"email":"mspagnolo27@gmail.com"}],"title":title,"html":html}
-      }, (err, res, body) => {
-         console.log(body);
-      });
-      }})
+        const title = `Birthday Reminder ${currentMonthName}`
+        request.post(process.env.TRUSTIFI_URL + '/api/i/v1/email', {
+        headers: {
+        'x-trustifi-key': process.env.TRUSTIFI_KEY,
+        'x-trustifi-secret': process.env.TRUSTIFI_SECRET
+      },
+      json:{"recipients":[{"email":"mspagnolo27@gmail.com"}],"title":title,"html":html}
+    }, (err, res, body) => {
+       console.log(body);
+    });
+    }})
+ }
+else{
+  console.log("No Email Sent not first of month")
+}
